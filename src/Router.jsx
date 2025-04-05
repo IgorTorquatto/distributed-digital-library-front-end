96a9f0b5635f110d4461fa7dac786fc50fe06493
Igor <igor.torquato.20@gmail.com>
2025-04-05 13:24:10 -0300

    add: rotas para páginas Home, Register, Login

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,10 +1,14 @@
 import { Routes,Route } from 'react-router-dom';
 import { Home } from './pages/Home';
+import { Register } from './pages/Register';
+import { Login } from './pages/Login';
 
 export const Router = () =>{
     return(
         <Routes>
-            <Route path='/' element={<Home/>}/>
+            <Route path='/' element={<Login/>}/>
+            <Route path='/home' element={<Home/>}/>
+            <Route path='/register' element={<Register/>}/>
         </Routes>
     )
 }
