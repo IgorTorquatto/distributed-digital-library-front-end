84ed6d84d4e17bc889061449cd389282bc00c1f0
Igor Torquato <igor.torquato.20@gmail.com>
2025-05-07 17:45:58 -0300

    att: integração com backend da página cadastro de livro

--- src/components/CardBook/CardBook.jsx
+++ src/components/CardBook/CardBook.jsx
@@ -5,7 +5,7 @@ export const CardBook = ({ image, title, author,showRightLine = false }) => {
   return (
     <>
         <div className={styles.card}>
-            <img src={image}/>
+            <img src={image} className={styles.cardImage}/>
             <div className={styles.bookDetails}>
                 <p className={styles.bookName}>{title}</p>
                 <p className={styles.bookAuthor}>{author}</p>
