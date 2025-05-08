46b06f9af178c0653c91985dc9166a3ce35e8df2
analuizatorres <maryclaer16@gmail.com>
2025-05-07 21:23:52 -0300

    login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,10 +1,36 @@
-import styles from './login.module.css'
+import styles from './login.module.css';
+import LoginNubi from '../../assets/nubi01_login.png';
+import NubiLogo from '../../assets/Nubi Logo.png'
+
 
 export const Login = () => {
 
   return (
     <>
-    <h1 className={styles.titulo}>login</h1>    
+    <div className={styles.Logincontainer}>
+      <div className={styles.LoginLeft}>
+        <img src={LoginNubi} alt="nubi_login" className={styles.NubiLoginImg}/>
+        {/*<h1>É bom tê-lo de volta!</h1>*/}
+      </div>
+
+      <div className={styles.loginRight}>
+        <img src={NubiLogo} alt="logo" className={styles.NubiLogoImg} />
+        <h2>Entrar</h2>
+        <p>Para ter acesso à sua conta, informe o seu endereço de E-mail e Senha!</p>
+
+        <form className={styles.loginForm}>
+          <input type="email" placeholder="Digite seu endereço de e-mail..."/>
+          <input type="password" placeholder="Digite sua senha..."/>
+          <a href="/register" className={styles.registerLink}>Não tem cadastro? Crie uma conta</a>
+          <button type="submit">Entrar</button>
+        </form>
+
+      </div>
+
+    </div>
+
+    
+    
     </>
   );
   
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,10 +1,47 @@
-import styles from './register.module.css'
+import styles from './register.module.css';
+import LoginNubi2 from '../../assets/nubi02_login.png';
+import NubiLogo from '../../assets/Nubi Logo.png'
+
 
 export const Register = () => {
 
   return (
     <>
-    <h1 className={styles.titulo}>register</h1>    
+    <div className={styles.Logincontainer}>
+      <div className={styles.LoginLeft}>
+        <img src={LoginNubi2} alt="nubi2_login" className={styles.NubiLoginImg}/>
+      </div>
+
+      <div className={styles.loginRight}>
+        <img src={NubiLogo} alt="logo" className={styles.NubiLogoImg} />
+        <h2>Inscrever-se</h2>
+        <p>Realize o seu cadastro e tenha acesso a todas as funcionalidades que o NUBI oferece!</p>
+
+        <div className={styles.formScrollable}>
+          <form className={styles.loginForm}>
+            <input type="email" placeholder="Digite seu endereço e-mail..." />
+            <input type="password" placeholder="Digite sua senha..." />
+            <input type="password" placeholder="Digite novamente a sua senha..." />
+            <input type="text" placeholder="Nome Completo" />
+            <input type="text" placeholder="Nome de Usuário" />
+            <input type="text" placeholder="CPF" />
+          </form>
+        </div>
+
+        <a href="/" className={styles.link}>Fazer Login</a>
+        <button type="submit" className={styles.button}>Confirmar</button>
+
+
+        <p className={styles.terms}>
+          Ao continuar você concorda com os <a>Termos de Serviço</a> e <a>Política de privacidade</a>
+        </p>
+
+      </div>
+
+    </div>
+
+    
+    
     </>
   );
   
